d9d091748127d0b4815d57933853197f44331405
RajRathinam <rajrathinam2005@gmail.com>
2024-11-15 04:53:23 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,107 @@
+function replaceAt(string, index, replace) {
+    return string.substring(0, index) + replace + string.substring(index + 1);
+}
+function flames() {
+    var body = document.querySelector("body");
+    var res = document.getElementById("output");
+    var a = document.getElementById("name1").value.toLowerCase();
+    var b = document.getElementById("name2").value.toLowerCase();
+    if (a != "" && b != "") {
+        for (i = 0; i < a.length; i++) {
+            for (j = 0; j < b.length; j++) {
+                if (a[i] == b[j]) {
+                    a = replaceAt(a, i, '*');
+                    b = replaceAt(b, j, '*');
+                }
+            }
+        }
+        var countLetters = 0;
+        for (i = 0; i < a.length; i++) {
+            if (a[i] != '*') {
+                countLetters++;
+            }
+        }
+        for (i = 0; i < b.length; i++) {
+            if (b[i] != '*') {
+                countLetters++;
+            }
+        }
+        if (countLetters > 1) {
+            var flames = "FLAMES";
+            c = 0;
+            l = 1;
+            for (i = 0; flames.length != 1; i++) {
+                if (l == countLetters) {
+                    if (c >= flames.length) {
+                        c = 0;
+                    }
+                    flames = replaceAt(flames, c, '');
+                    l = 1;
+                }
+                if (c >= flames.length) {
+                    c = 0;
+                }
+                l++;
+                c++;
+            }
+
+            switch (flames) {
+                case 'F':
+                    flames = "Friend";
+                    body.style.backgroundImage = "url('./assets/friend.jpeg')";
+                    body.style.backgroundSize = "cover"; // Makes the image cover the entire background
+                    body.style.backgroundRepeat = "no-repeat"; // Prevents the image from repeating
+                    body.style.backgroundPosition = "center"; // Centers the image
+                    break;
+                case 'L':
+                    flames = "Love";
+                    body.style.backgroundImage = "url('./assets/love.jpeg')";
+                    body.style.backgroundSize = "cover"; // Makes the image cover the entire background
+                    body.style.backgroundRepeat = "no-repeat"; // Prevents the image from repeating
+                    body.style.backgroundPosition = "center"; // Centers the image
+                    break;
+                case 'A':
+                    flames = "Affection";
+                    body.style.backgroundImage = "url('./assets/affection.jpeg')";
+                    body.style.backgroundSize = "cover"; // Makes the image cover the entire background
+                    body.style.backgroundRepeat = "no-repeat"; // Prevents the image from repeating
+                    body.style.backgroundPosition = "center"; // Centers the image
+                    break;
+                case 'M':
+                    flames = "Marriage";
+                    body.style.backgroundImage = "url('./assets/marriage.jpeg')";
+                    body.style.backgroundSize = "cover"; // Makes the image cover the entire background
+                    body.style.backgroundRepeat = "no-repeat"; // Prevents the image from repeating
+                    body.style.backgroundPosition = "center"; // Centers the image
+                    break;
+                case 'E':
+                    flames = "Enemies";
+                    body.style.backgroundImage = "url('./assets/enemy.jpeg')";
+                    body.style.backgroundSize = "cover"; // Makes the image cover the entire background
+                    body.style.backgroundRepeat = "no-repeat"; // Prevents the image from repeating
+                    body.style.backgroundPosition = "center"; // Centers the image
+                    break;
+                case 'S':
+                    flames = "Sibling";
+                    body.style.backgroundImage = "url('./assets/siblings.jpeg')";
+                    body.style.backgroundSize = "cover"; // Makes the image cover the entire background
+                    body.style.backgroundRepeat = "no-repeat"; // Prevents the image from repeating
+                    body.style.backgroundPosition = "center"; // Centers the image
+                    break;
+            }
+        }
+        if (countLetters == 1) {
+            flames = "Sibling";
+        }
+        if (countLetters == 0) {
+            flames = "Its Same Name !!!";
+            body.style.backgroundImage = "url('./assets/main_body.jpeg')";
+            body.style.backgroundSize = "cover"; // Makes the image cover the entire background
+            body.style.backgroundRepeat = "no-repeat"; // Prevents the image from repeating
+            body.style.backgroundPosition = "center"; // Centers the image
+        }
+        res.innerHTML = "<b style='color:blue;'>" + document.getElementById("name2").value + "</b> is Your <b style='color:red;'>" + flames + "</b>";
+    } else {
+        res.innerHTML = "Please Enter Name";
+    }
+}
